ae20d32b4fb5d3d08c96eb37e0c0ed9caf041037
Jesus <jesusdavidgonzales1102@gmail.com>
2023-01-22 07:21:04 -0500

    api consumption

--- src/components/seeker.js
+++ src/components/seeker.js
@@ -1,11 +1,18 @@
 import React from "react";
 import '../sass/Seeker.scss'
 
-const Seeker = () => (
-    <div className="container_buscador">
-       <input type='text' placeholder="Buscar un Pokemon" />
-       <button></button> 
-    </div>
-)
+function Seeker({ filtro }) {
+
+    const handleOnchange = (e) => {
+        filtro(e.target.value)
+    }
+
+    return (
+        <div className="container_buscador">
+            <input type='text' placeholder="Buscar un Pokemon" onChange={(e)=>handleOnchange(e)} />
+            <button></button> 
+        </div>
+    )
+}
 
 export default Seeker
--- src/pages/Pokedex.js
+++ src/pages/Pokedex.js
@@ -1,17 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/header";
 import Seeker from "../components/seeker";
-import Pantalla from "../components/pantalla";
 import Footer from "../components/footer";
 import "../sass/Pokedex.scss"
 
-const Pokedex = () => (
-    <div className="Pokedex">
-        <Header />
-        <Seeker />
-        <Pantalla />
-        <Footer />
-    </div>
-);
+function Pokedex (){
+
+    const [pokemon, setpokemon] = useState([])
+    const [filter, setFilter] = useState([])
+
+    useEffect(()=>{
+        async function CallApi() {
+            const urlApi = await fetch(`https://pokeapi.co/api/v2/pokemon/${filter}`)
+            console.log(urlApi)
+            const Json = await urlApi.json() 
+            setpokemon(Json)
+        }
+        CallApi()
+    }, [filter])
+
+    function Userfilter(busqueda) {
+        setFilter(busqueda)
+    }
+    
+    return (
+        <div className="Pokedex">
+            <Header />
+            <Seeker filtro={Userfilter} />
+            <div className="Pantalla">
+                <div className="image-pokemon">
+                    <img src={ '' /* pokemon.sprites.front_default */} alt='pokemon' />
+                </div>
+                <div className="caractertisticas">
+                    <div className="caracteristica_pokemon">
+                        <p>Nombre: </p>
+                        <p>{pokemon.name}</p>
+                    </div>
+                    <div className="caracteristica_pokemon">
+                        <p>Habilidad: </p>
+                        <p>{/* pokemon.abilities[1].ability.name */}</p>
+                    </div>
+                    <div className="caracteristica_pokemon">
+                        <p>Ataque: </p>
+                        <p>{/* pokemon.stats[1].base_stat */}</p>
+                    </div>
+                    <div className="caracteristica_pokemon">
+                        <p>Defensa: </p>
+                        <p>{/* pokemon.stats[2].base_stat */}</p>
+                    </div>
+                    <div className="caracteristica_pokemon">
+                        <p>Base experiencia: </p>
+                        <p>{/* pokemon.base_experience */}</p>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    )
+};
 
 export default Pokedex
